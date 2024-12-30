3fc5f55e9d89457f2273b17e9d92d31af7509f45
majidmohammadian <majeedmohammadian@gmail.com>
2024-12-30 20:21:04 +0330

    added config file

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Time
+    |--------------------------------------------------------------------------
+    |
+    | Cache time for get data authio
+    |
+    | - set zero for remove cache
+    | - set null for forever
+    |
+    | - unit: minutes
+    */
+
+    "cache_time" => env("AUTHIO_CACHE_TIME", 0),
+
+];
