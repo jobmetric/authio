a21fd18be11f7cadf3b393996717ee1b87aa5ccd
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-03 22:35:24 +0330

    added auth request file

--- src/Http/Requests/AuthRequestRequest.php
+++ src/Http/Requests/AuthRequestRequest.php
@@ -4,7 +4,9 @@ namespace JobMetric\Authio\Http\Requests;
 
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 use JobMetric\Authio\Enums\LoginTypeEnum;
+use JobMetric\Authio\Rules\MobileRule;
 
 class AuthRequestRequest extends FormRequest
 {
@@ -23,13 +25,61 @@ class AuthRequestRequest extends FormRequest
      */
     public function rules(): array
     {
+        $user_table = config('authio.tables.user');
         return [
-            'type' => 'string|in:' . implode(',', LoginTypeEnum::values()),
+            'type' => [
+                'required',
+                'string',
+                'in:' . implode(',', LoginTypeEnum::values())
+            ],
 
-            'mobile_prefix' => 'required_if:type,==,' . LoginTypeEnum::MOBILE(),
-            'mobile' => 'required_if:type,==,' . LoginTypeEnum::MOBILE(),
+            'mobile_prefix' => [
+                'nullable',
+                'required_if:type,==,' . LoginTypeEnum::MOBILE(),
+                'string',
+                'max:20'
+            ],
+            'mobile' => [
+                'nullable',
+                'required_if:type,==,' . LoginTypeEnum::MOBILE(),
+                'string',
+                'max:20',
+                new MobileRule('mobile_prefix')
+            ],
 
-            'email' => 'required_if:type,==,' . LoginTypeEnum::EMAIL(),
+            'email' => [
+                'nullable',
+                'required_if:type,==,' . LoginTypeEnum::EMAIL(), 'email',
+                'max:255',
+                'unique:' . $user_table . ',email'
+            ],
+
+            'hash' => [
+                'nullable',
+                'string',
+                'max:255'
+            ],
         ];
+    }
+
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation(): void
+    {
+        $type = $this->input('type');
+
+        if ($type == LoginTypeEnum::MOBILE()) {
+            $this->merge([
+                'email' => null
+            ]);
+        } elseif ($type == LoginTypeEnum::EMAIL()) {
+            $this->merge([
+                'mobile_prefix' => null,
+                'mobile' => null
+            ]);
+        }
     }
 }
