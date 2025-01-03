b4bca73823c48f3b81626f46e1a4cfbb6a6621db
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-04 00:23:23 +0330

    changed js file for new algorithm

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,156 +1,179 @@
-// Timer
-let interval = null
-function startTimer (timer) {
-    if(interval) {
-        clearInterval(interval)
-    }
-    $('#timer').text(timer);
-    interval = setInterval(function () {
-        $('#timer').text(--timer);
+"use strict"
 
-        if (!timer) {
-            clearInterval(interval)
-            $('#timer').removeClass('active')
-            $('#btn-resend').addClass('active')
-        }
-    }, 1000);
-}
-
-toastr.options = {
-    "closeButton": false,
-    "debug": true,
-    "newestOnTop": true,
-    "progressBar": false,
-    "positionClass": "toastr-top-center",
-    "preventDuplicates": true,
-    "onclick": null,
-    "showDuration": "300",
-    "hideDuration": "1000",
-    "timeOut": "5000",
-    "extendedTimeOut": "1000",
-    "showEasing": "swing",
-    "hideEasing": "linear",
-    "showMethod": "fadeIn",
-    "hideMethod": "fadeOut"
-};
-
-$(document).ready(function (){
-    // form auth
-    // change mobile or email
-    $(document).on('click', '.form-auth-change-data', function (e){
-        e.preventDefault()
-        $('.form-auth-data-mode,.form-auth-change-data').removeClass('active')
-        $(this).addClass('active')
-        $('#form-auth-'+$(this).data('type')).addClass('active')
-    })
-
-    // submit form auth
-    $(document).on('click', '#btn-next', function (e){
-        e.preventDefault()
-        $.ajax({
-            url: localize.auth.route.request,
-            method: 'post',
-            dataType: 'json',
-            data: {
-                _token: getCsrfToken(),
-                type: $('.form-auth-change-data.active').data('type'),
-                mobile_prefix: $('#mobile-prefix').val(),
-                mobile: $('#mobile').val(),
-                email: $('#email').val(),
+const authio = {
+    init: {
+        ready: function () {
+            authio.config.toast()
+        },
+    },
+    pages: {
+        auth: {
+            type: 'mobile',
+            changeType: function(element, e){
+                e.preventDefault()
+                $('.form-auth-data-mode,.form-auth-change-data').removeClass('active')
+                $(element).addClass('active')
+                let elementType = $(element).data('type')
+                $('#form-auth-' + elementType).addClass('active')
+                authio.pages.auth.type = elementType
             },
-            beforeSend: function(){
-                // $('#loading').addClass('active')
-                $('.form-error').removeClass('active').text('')
-            },
-            complete: function(){
-                $('#loading').removeClass('active')
-            },
-            success: function(json){
-
-            },
-            error: function(res){
-                toastr.error(res.responseJSON.message)
-                $.each(res.responseJSON.errors, function (field, value){
-                    $('#error-' + field).addClass('active').text(value)
+            send: function(e){
+                e.preventDefault()
+                $.ajax({
+                    url: localize.auth.route.request,
+                    method: 'post',
+                    dataType: 'json',
+                    data: {
+                        _token: getCsrfToken(),
+                        type: authio.pages.auth.type,
+                        mobile_prefix: $('#mobile-prefix').val(),
+                        mobile: $('#mobile').val(),
+                        email: $('#email').val(),
+                    },
+                    beforeSend: function(){
+                        // $('#loading').addClass('active')
+                        $('.form-error').removeClass('active').text('')
+                    },
+                    complete: function(){
+                        $('#loading').removeClass('active')
+                    },
+                    success: function(json){
+                        $('.form-data').removeClass('active')
+                        $('#form-code').addClass('active')
+                        $('#code-1').focus()
+                    },
+                    error: function(res){
+                        toastr.error(res.responseJSON.message)
+                        $.each(res.responseJSON.errors, function (field, value){
+                            $('#error-' + field).addClass('active').text(value)
+                        })
+                    },
                 })
             },
-        })
-        // $('.form-data').removeClass('active')
-        // $('#form-code').addClass('active')
-        // $('#code-1').focus()
-        // startTimer(5)
-    })
-
-    // form code
-    $(document).on('click', '#back-form-auth', function (e){
-        e.preventDefault()
-        $('.form-data').removeClass('active')
-        $('#form-auth').addClass('active')
-    })
-
-    // change code number
-    let backspaceKey = 8, deleteKey = 46, tabKey = 9, enterKey = 13, escapeKey = 27, shiftKey = 16, leftKey = 37, rightKey = 39
-    let validKeys = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 8, 46, 9, 13, 27, 16, 37, 39]
-    let numberKeys = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105]
-    let maxCode = 5
-
-    $(document).on('keydown', '.form-code-number', function (e){
-        let element_number = $(this).data('code-number')
-        if ($.inArray(e.keyCode, validKeys) === -1) {
-            e.preventDefault()
-            return
-        }
-        // shift + tab, backspace, delete
-        if ((e.shiftKey && e.keyCode === tabKey)) {
-            if (element_number > 1) {
-                $('#code-' + (element_number - 1)).focus()
+            backTo: function(e){
                 e.preventDefault()
-                return
-            }
-        }
+                $('.form-data').removeClass('active')
+                $('#form-auth').addClass('active')
+            },
+        },
+        code: {
+            fill: function (element, e){
+                let backspaceKey = 8;
+                let deleteKey = 46;
+                let tabKey = 9;
+                let enterKey = 13;
+                let escapeKey = 27;
+                let leftKey = 37;
+                let rightKey = 39;
+                let validKeys = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 8, 46, 9, 13, 27, 16, 37, 39];
+                let numberKeys = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105];
+                let maxCode = 5;
 
-        if (e.keyCode === backspaceKey || e.keyCode === deleteKey || e.keyCode === escapeKey) {
-            $(this).val('')
-            setTimeout(function (){
-                if (element_number > 1) {
-                    $('#code-' + (element_number - 1)).focus()
+                let element_number = $(element).data('code-number')
+                if ($.inArray(e.keyCode, validKeys) === -1) {
                     e.preventDefault()
+                    return
                 }
-            }, 30)
-            return
-        }
-
-        // left key
-        if (e.keyCode === leftKey) {
-            if (element_number > 1) {
-                $('#code-' + (element_number - 1)).focus()
-                e.preventDefault()
-                return
-            }
-        }
-        // right key
-        if (e.keyCode === rightKey || e.keyCode === tabKey) {
-            if (element_number < maxCode) {
-                $('#code-' + (element_number + 1)).focus()
-                e.preventDefault()
-                return
-            }
-        }
-        if (e.keyCode === enterKey && $(this).data('code-number') === 5) {
-            // submit code
-            e.preventDefault()
-            return
-        }
-
-        if ($.inArray(e.keyCode, numberKeys) !== -1) {
-            $(this).val($(this).val().substring(0, 0))
-            setTimeout(function(){
-                if (element_number < maxCode) {
-                    $('#code-' + (element_number + 1)).focus()
+                // shift + tab, backspace, delete
+                if ((e.shiftKey && e.keyCode === tabKey)) {
+                    if (element_number > 1) {
+                        $('#code-' + (element_number - 1)).focus()
+                        e.preventDefault()
+                        return
+                    }
                 }
-            },30)
-        }
-    })
+
+                if (e.keyCode === backspaceKey || e.keyCode === deleteKey || e.keyCode === escapeKey) {
+                    $(element).val('')
+                    setTimeout(function (){
+                        if (element_number > 1) {
+                            $('#code-' + (element_number - 1)).focus()
+                            e.preventDefault()
+                        }
+                    }, 30)
+                    return
+                }
+
+                // left key
+                if (e.keyCode === leftKey) {
+                    if (element_number > 1) {
+                        $('#code-' + (element_number - 1)).focus()
+                        e.preventDefault()
+                        return
+                    }
+                }
+                // right key
+                if (e.keyCode === rightKey || e.keyCode === tabKey) {
+                    if (element_number < maxCode) {
+                        $('#code-' + (element_number + 1)).focus()
+                        e.preventDefault()
+                        return
+                    }
+                }
+                if (e.keyCode === enterKey && $(element).data('code-number') === 5) {
+                    // submit code
+                    e.preventDefault()
+                    return
+                }
+
+                if ($.inArray(e.keyCode, numberKeys) !== -1) {
+                    $(element).val($(element).val().substring(0, 0))
+                    setTimeout(function(){
+                        if (element_number < maxCode) {
+                            $('#code-' + (element_number + 1)).focus()
+                        }
+                    },30)
+                }
+            },
+            send: function(){}
+        },
+        password: {},
+        authenticator: {},
+        selection: {},
+    },
+    config: {
+        toast: function(){
+            toastr.options = {
+                "closeButton": false,
+                "debug": true,
+                "newestOnTop": true,
+                "progressBar": false,
+                "positionClass": "toastr-top-center",
+                "preventDuplicates": true,
+                "onclick": null,
+                "showDuration": "300",
+                "hideDuration": "1000",
+                "timeOut": "5000",
+                "extendedTimeOut": "1000",
+                "showEasing": "swing",
+                "hideEasing": "linear",
+                "showMethod": "fadeIn",
+                "hideMethod": "fadeOut"
+            };
+        },
+        timer: {
+            interval: null,
+            start: function (timer) {
+                if(authio.config.timer.interval) {
+                    clearInterval(authio.config.timer.interval)
+                }
+                $('#timer').text(timer);
+                authio.config.timer.interval = setInterval(function () {
+                    $('#timer').text(--timer);
+
+                    if (!timer) {
+                        clearInterval(authio.config.timer.interval)
+                        $('#timer').removeClass('active')
+                        $('#btn-resend').addClass('active')
+                    }
+                }, 1000);
+            }
+        },
+    },
+}
+
+$(document).ready(function (){
+
 
     $(document).on('click', '#btn-resend', function (e){
         $('#btn-resend').removeClass('active')
