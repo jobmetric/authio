4758364a758fc5867c01ba7496b454a3af332247
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-03 22:31:18 +0330

    added some config parameter

--- config/config.php
+++ config/config.php
@@ -41,4 +41,40 @@ return [
 
     "background_image" => env("AUTHIO_BACKGROUND_IMAGE", "bg7"),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Enable Send SMS
+    |--------------------------------------------------------------------------
+    |
+    | Enable send sms for authio
+    */
+
+    "enable_send_sms" => env("AUTHIO_ENABLE_SEND_SMS", true),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enable Send Email
+    |--------------------------------------------------------------------------
+    |
+    | Enable send email for authio
+    */
+
+    "enable_send_email" => env("AUTHIO_ENABLE_SEND_EMAIL", true),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Otp Code
+    |--------------------------------------------------------------------------
+    |
+    | Otp code for authio
+    */
+
+    'otp' => [
+        'default_value' => env('AUTHIO_OTP_DEFAULT_VALUE', 12345),
+        'try_count' => env('AUTHIO_OTP_TRY_COUNT', 5),
+        'expire_reuse' => env('AUTHIO_OTP_EXPIRE_REUSE', 60),
+        'expire_time' => env('AUTHIO_OTP_EXPIRE_TIME', 300),
+        'ban_ip' => env('AUTHIO_OTP_BAN_IP', 600),
+    ],
+
 ];
