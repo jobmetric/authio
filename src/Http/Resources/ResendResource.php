bcf088af02766f421a38a149256f6089137d18a2
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-04 23:09:13 +0330

    added resend resource file

--- src/Http/Resources/ResendResource.php
+++ src/Http/Resources/ResendResource.php
@@ -5,7 +5,7 @@ namespace JobMetric\Authio\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class RequestResource extends JsonResource
+class ResendResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -16,8 +16,6 @@ class RequestResource extends JsonResource
     {
         return [
             'source' => $this['source'],
-            'type' => $this['type'],
-            'has_password' => $this['has_password'],
             'secret' => $this['secret'],
             'code' => $this['code'] ?? null,
         ];
