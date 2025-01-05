f5c1459a1f4e1c4db61afac247fc9af171e4590f
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-05 23:55:42 +0330

    added some change to auth config in service provider and load jwt package in authio service provider

--- src/AuthioServiceProvider.php
+++ src/AuthioServiceProvider.php
@@ -2,13 +2,16 @@
 
 namespace JobMetric\Authio;
 
+use JobMetric\Authio\Models\User;
 use JobMetric\PackageCore\Enums\RegisterClassTypeEnum;
 use JobMetric\PackageCore\Exceptions\AssetFolderNotFoundException;
+use JobMetric\PackageCore\Exceptions\DependencyPublishableClassNotFoundException;
 use JobMetric\PackageCore\Exceptions\MigrationFolderNotFoundException;
 use JobMetric\PackageCore\Exceptions\RegisterClassTypeNotFoundException;
 use JobMetric\PackageCore\Exceptions\ViewFolderNotFoundException;
 use JobMetric\PackageCore\PackageCore;
 use JobMetric\PackageCore\PackageCoreServiceProvider;
+use Tymon\JWTAuth\Providers\LaravelServiceProvider;
 
 class AuthioServiceProvider extends PackageCoreServiceProvider
 {
@@ -17,6 +20,7 @@ class AuthioServiceProvider extends PackageCoreServiceProvider
      * @throws ViewFolderNotFoundException
      * @throws AssetFolderNotFoundException
      * @throws MigrationFolderNotFoundException
+     * @throws DependencyPublishableClassNotFoundException
      */
     public function configuration(PackageCore $package): void
     {
@@ -27,6 +31,38 @@ class AuthioServiceProvider extends PackageCoreServiceProvider
             ->hasView()
             ->hasAsset()
             ->hasRoute()
-            ->registerClass('Authio', Authio::class, RegisterClassTypeEnum::SINGLETON());
+            ->registerClass('Authio', Authio::class, RegisterClassTypeEnum::SINGLETON())
+            ->registerDependencyPublishable(LaravelServiceProvider::class, 'config');
+    }
+
+    /**
+     * Before Register Package
+     *
+     * @return void
+     */
+    public function beforeRegisterPackage(): void
+    {
+        $this->app->register(LaravelServiceProvider::class);
+    }
+
+    /**
+     * After Boot Package
+     *
+     * @return void
+     */
+    public function afterBootPackage(): void
+    {
+        // load authio guard
+        config()->set("auth.guards.authio", [
+            'driver' => 'jwt',
+            'provider' => 'authio',
+        ]);
+
+        config()->set("auth.providers.authio", [
+            'users' => [
+                'driver' => 'eloquent',
+                'model' => User::class,
+            ],
+        ]);
     }
 }
